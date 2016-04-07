ad999d5c7190fe8ba7454954a893e3e769a336be
Tomas Yanez <tyanez@lclark.edu>
2016-04-06 21:04:10 -0700

    user

--- app/src/main/java/lclark/mapplication/User.java
+++ app/src/main/java/lclark/mapplication/User.java
@@ -1,14 +1,12 @@
 package lclark.mapplication;
 
 import android.content.ContentValues;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.provider.BaseColumns;
 
 /**
  * Created by larspmayrand on 4/3/16.
  */
-public class User implements BaseColumns, Parcelable {
+public class User implements BaseColumns {
 
     private String name;
     private int idNumber;
@@ -20,30 +18,6 @@ public class User implements BaseColumns, Parcelable {
     public static final String CREATE_TABLE = "CREATE TABLE" + TABLE_NAME + " ( " +
             _ID + " TEXT PRIMARY KEY, " +
             COLUMN_NAME + " TEXT, ";
-
-    protected User(Parcel source) {
-
-    }
-
-    protected GithubUser(Parcel in) {
-        login = in.readString();
-        id = in.readInt();
-        avatar_url = in.readString();
-        gravatar_id = in.readString();
-        url = in.readString();
-        html_url = in.readString();
-        followers_url = in.readString();
-        following_url = in.readString();
-        gists_url = in.readString();
-        starred_url = in.readString();
-        subscriptions_url = in.readString();
-        organizations_url = in.readString();
-        repos_url = in.readString();
-        events_url = in.readString();
-        received_events_url = in.readString();
-        type = in.readString();
-        site_admin = in.readByte() != 0;
-    }
 
     public String getName() {
         return name;
@@ -66,41 +40,6 @@ public class User implements BaseColumns, Parcelable {
         ContentValues contentValues = new ContentValues();
         contentValues.put(COLUMN_NAME, name);
         return contentValues;
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(name);
-        dest.writeInt(idNumber);
-    }
-
-//    public static final Creator<GithubUser> CREATOR = new Creator<GithubUser>() {
-//        @Override
-//        public GithubUser createFromParcel(Parcel in) {
-//            return new GithubUser(in);
-//        }
-//
-//        @Override
-//        public GithubUser[] newArray(int size) {
-//            return new GithubUser[size];
-//        }
-//    };
-
-    public static final Creator<User> CREATOR = new Creator<User>() {
-        @Override
-        public User createFromParcel(Parcel source) {
-            return new User(source);
-        }
-
-        @Override
-        public User[] newArray(int size) {
-            return new User[0];
-        }
     }
 
 }
